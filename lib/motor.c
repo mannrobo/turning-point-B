2db3fd6229ebc12b0d567fafbf3d7e009160a06b
MayorMonty <artificalchamber@gmail.com>
2018-10-20 23:57:05 -0400

    Import libraries from last year

---
+++ lib/motor.c
@@ -0,0 +1,83 @@
+/**
+ * motor.c - Improved motor control including slew rate, truespeed, deadband, and motor groups. Includes PID control for motors as well
+ */
+
+#include "util.c"
+
+// Stores motor targets, use this instead of motor[]
+int motorTarget[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int motorSlew[10] =   { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }; 
+int motorSlewLastSet[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int motorDeadband[10] = { 40, 40, 15, 15, 15, 15, 15, 15, 15, 15 };
+
+// Thanks to Jess from 21S (@Unionjackz) for truespeed tables
+static const char L298[128] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 11, 11, 12,
+    12, 12, 12, 13, 13, 13, 14, 14, 14, 14,
+    15, 15, 15, 16, 16, 16, 17, 17, 17, 18,
+    18, 18, 19, 19, 19, 20, 20, 20, 21, 21,
+    22, 22, 23, 23, 23, 24, 25, 25, 26, 26,
+    26, 27, 27, 28, 28, 29, 29, 30, 30, 31,
+    31, 32, 32, 33, 33, 34, 35, 35, 35, 35,
+    35, 35, 38, 38, 39, 41, 41, 41, 42, 43,
+    45, 46, 47, 47, 48, 49, 49, 50, 51, 52,
+    53, 54, 55, 56, 58, 59, 63, 66, 67, 70,
+    73, 74, 75, 78, 80, 84, 87, 88, 92, 95,
+    97, 100, 105, 108, 114, 117, 121, 122, 124, 127
+};
+
+static const char MC29[128] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 13, 13, 14,
+    14, 14, 14, 15, 15, 15, 15, 16, 16, 16,
+    16, 16, 17, 17, 17, 17, 17, 17, 17, 17,
+    18, 18, 18, 18, 18, 18, 18, 19, 19, 19,
+    19, 19, 20, 20, 20, 20, 21, 21, 21, 22,
+    22, 22, 22, 23, 23, 23, 23, 24, 24, 24,
+    25, 25, 25, 26, 26, 26, 27, 27, 27, 28,
+    28, 28, 29, 29, 30, 30, 30, 31, 31, 32,
+    32, 33, 33, 34, 34, 35, 36, 36, 37, 37,
+    38, 39, 40, 40, 41, 42, 43, 44, 45, 46,
+    46, 48, 49, 50, 51, 53, 55, 56, 58, 60,
+    62, 64, 67, 70, 72, 76, 79, 83, 84, 127,
+};
+
+void motorHandle() {
+    int outs[10]; // Stores intermediate output values
+    int motorCurrent; // Temp variable used for slew rate
+    // Loop through each motor slot
+    for(int i = 0; i < 10; i++) {
+
+        // 1. Target
+        outs[i] = clamp(motorTarget[i], -127, 127);
+
+        // 2. Deadband
+        if (motorDeadband[i] > abs(outs[i])) {
+            outs[i] = 0;
+        }
+
+        // 3. Slew Rate - Gradually increases motor power, reducing the chance of PTC trips
+        motorCurrent = motorSlewLastSet[i];
+        if(motorCurrent != outs[i]) {
+            motorCurrent +=
+            sgn(outs[i] - motorCurrent) * // Whether to increase or decrease in value
+            clamp(motorSlew[i], 0, abs(outs[i] - motorCurrent)); // The amount to increase, the clamp prevents the value from being greater than the difference remaining
+        }
+
+        outs[i] = motorCurrent;
+        motorSlewLastSet[i] = outs[i];
+
+        // 4. TrueSpeed - Standardizes the acceleration curve of the Motor Controller
+        if(i == 0 || i == 9) {
+            // outs[i] = sgn(outs[i]) * L298[abs(outs[i])];
+        } else {
+            // outs[i] = sgn(outs[i]) * MC29[abs(outs[i])];
+        }
+
+        // writeDebugStreamLine("port%d: %d", i, outs[i]);
+
+        // 5. Set Motor
+        motor[i] = outs[i];
+    }
+}
---
+++ lib/util.c
@@ -0,0 +1,135 @@
+/**
+ * util.c - General purpose utility functions, like those that might be found in C's standard library (but are missing in RobotC, grr!)
+ */
+
+#pragma systemFile
+
+#define arraySize(a) (sizeof(a)/sizeof(a[0]))
+#define PI 3.1415926535
+
+/**
+ * Performs modulus for floats
+ * @param float x The numerator
+ * @param float y The demominator
+ * @return float The remainder of x / y
+ */
+float fmodf(float x, float y) {
+  return (x - y * floor(x / y));
+}
+
+/**
+ * Clamps a value to a maximum and minimum
+ * @param float val The value to clamp
+ * @param float min The minimum value for the value
+ * @param float max The maximum value for the value
+ * @return float The clamped value
+ */
+float clamp(float val, float min, float max) {
+  return val > max ? max : val < min ? min : val;
+}
+
+/**
+ * Corrects a single direction encoder, giving it the corrected value, based on the specified associated motor
+ * @param int lastValue The last returned value of this function
+ * @param int measuredValue The new measured value of the sensor
+ * @param int pwm The pwm of the motor the sensor is attached to
+ * @return int The correct position of the encoder
+ * EXAMPLE:
+ *
+ * int encoderValue = SensorValue[encoder];
+ * while(true) {
+ *  encoderValue = encoderDirect(encoderValue, SensorValue[encoder], motor[port3]);
+ *  wait1Msec(20);
+ * }
+ *
+ * NOTE: This function will not be nearly as accurate as merely using a correct
+ * 2 direction encoder, it is meant as a backup only!
+ */
+int encoderDirect(int lastValue, int measuredValue, int pwm) {
+  return lastValue + sgn(pwm) * (measuredValue - lastValue);
+}
+
+/**
+ * Rescales two values to a specified maximum and returns the specified value
+ * @param int max The maximum value, used to calculate the scalar
+ * @param int alpha The first value to rescale
+ * @param int beta The second value to rescale
+ * @param int side The value to return, 0 for alpha, 1 for beta
+ **/
+int rescaleTo(int max, int alpha, int beta, int side) {
+  if (alpha <= max && beta <= max) return side ? beta : alpha; // If no sclaing is needed, return
+  
+  float scalar = alpha > beta ? (float) max / (float) alpha : (float) max / (float) beta;
+  return (int)(side ? beta * scalar : alpha * scalar);
+}
+
+
+
+#define TURBO 2.4
+#define HIGHSPEED 1.6
+#define TORQUE 1
+
+/**
+ * Converts inches to quadrature encoder ticks, for the purpose of setting targets in PIDs
+ * Usage:
+ *  inchesToTicks(10, 3.25, 1, TURBO);
+ *
+ * @param float unit The number inches to convert to ticks
+ * @param float wheelDiameter The diameter of the wheel, in inches
+ * @param float gearing The external gear ration (Speed Up => x, Torque Up => 1/x)
+ * @param float motorGear The internal gear ratio of the motor (constants TURBO, HIGHSPEED, and TORQUE have been specified)
+ */
+float inchesToTicks(float unit, float wheelDiameter, float gearing, float motorGear) {
+    return (
+        (unit / (motorGear * gearing)) / // Cancel out gear ratio
+        (wheelDiameter * PI) // Divide by circumfrence to solve for rotations
+    ) * 360; // 360 ticks in a rotation
+}
+
+#define STRTOK_MAX_TOKEN_SIZE 20
+#define STRTOK_MAX_BUFFER_SIZE 50
+
+/**
+ * Tokenise an array of chars, using a seperator
+ * @param buffer pointer to buffer we're parsing
+ * @param token pointer to buffer to hold the tokens as we find them
+ * @param seperator the seperator used between tokens
+ * @return true if there are still tokens left, false if we're done
+ */
+bool strtok(char *buffer, char *token, char *seperator)
+{
+  int pos = stringFind(buffer, seperator);
+  char t_buff[STRTOK_MAX_BUFFER_SIZE];
+
+  // Make sure we zero out the buffer and token
+  memset(token, 0, STRTOK_MAX_TOKEN_SIZE);
+  memset(&t_buff[0], 0, STRTOK_MAX_BUFFER_SIZE);
+
+  // Looks like we found a seperator
+  if (pos >= 0)
+  {
+    // Copy the first token into the token buffer, only if the token is
+    // not an empty one
+    if (pos > 0)
+      memcpy(token, buffer, pos);
+    // Now copy characters -after- the seperator into the temp buffer
+    memcpy(&t_buff[0], buffer+(pos+1), strlen(buffer) - pos);
+    // Zero out the real buffer
+    memset(buffer, 0, strlen(buffer) + 1);
+    // Copy the temp buffer, which now only contains everything after the previous
+    // token into the buffer for the next round.
+    memcpy(buffer, &t_buff[0], strlen(&t_buff[0]));
+    return true;
+  }
+  // We found no seperator but the buffer still contains a string
+  // This can happen when there is no trailing seperator
+  else if(strlen(buffer) > 0)
+  {
+    // Copy the token into the token buffer
+    memcpy(token, buffer, strlen(buffer));
+    // Zero out the remainder of the buffer
+    memset(buffer, 0, strlen(buffer) + 1);
+    return true;
+  }
+  return false;
+}
