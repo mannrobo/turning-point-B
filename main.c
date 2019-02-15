501785cabb3d63388bf74b6ea51d44e0ef5ccfd7
MayorMonty <artificalchamber@gmail.com>
2019-02-15 11:20:54 -0500

    Formatting

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #pragma config(UART_Usage, UART1, uartVEXLCD, baudRate19200, IOPins, None, None)
 #pragma config(UART_Usage, UART2, uartNotUsed, baudRate4800, IOPins, None, None)
 #pragma config(Sensor, in1,    gyro,           sensorGyro)
